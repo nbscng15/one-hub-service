ea22249e216ca4e0ff4fa6becc3647b1502af55a
Nebcarl Secong <nbscng@gmail.com>
2026-09-05 23:35:36 +0800

    Initial commit

---
+++ src/main/java/com/benifex/one_hub_service/OneHubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.benifex.one_hub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneHubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benifex/one_hub_service/OneHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benifex.one_hub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneHubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
